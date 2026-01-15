47682dc12b382180a2f0c5ff072adbeb879b8875
Anderi0n <136240312+Anderi0n@users.noreply.github.com>
2026-01-15 21:15:15 +0200

    Add files via upload

---
+++ teht_2.4.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Anna numero 1: "))
+numero2 = int(input("Anna numero 2: "))
+numero3 = int(input("Anna numero 3: "))
+
+summa = numero1 + numero2 + numero3
+tulo = numero1 * numero2 * numero3
+keskiarvo = numero1 /numero2 / numero3
+print(f" Lukujen summa on {summa}. Lukujen tulo on {tulo} ja Lukujen keskiarvo on {keskiarvo}")
