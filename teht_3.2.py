ed08ea19d9d7534e225c1205d9fe2c6f542f6e62
saif <saif@gmail.com>
2026-01-17 14:39:27 +0200

    teht3.2

---
+++ teht_3.2.py
@@ -0,0 +1,16 @@
+from http.cookiejar import uppercase_escaped_char
+
+lux = input("Anna hyttiluokka: ")
+upperlux = lux.upper()
+if lux == ("lux"):
+    print(f" {upperlux} on parvekkeellinen hytti yläkannella.")
+elif lux == ("A"):
+    print(f"{upperlux} on ikkunallinen hytti autokannen yläpuolella.")
+elif lux == ("B"):
+    print(f"{upperlux} on ikkunaton hytti autokannen yläpuolella.")
+elif lux == ("C"):
+    print(f"{upperlux} on ikkunaton hytti autokannen alapuolella.")
+if lux not in ("lux", "A", "B", "C"):
+    print("Virheellinen hyttiluokka.")
+
+
