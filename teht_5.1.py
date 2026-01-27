f7dd2d3684dee8dc20bc7d3106a92a0f2aa7f659
saif <saif@gmail.com>
2026-01-27 10:54:00 +0200

    noppa

---
+++ teht_5.1.py
@@ -0,0 +1,13 @@
+import random
+list = ["david", "eddie", "ander"]
+
+times = int(input("Dice rolls how many?: "))
+total = 0
+
+for i in range(times):
+    random_num = random.randint(1, 6)
+    total = total + random_num
+    order = i + 1
+    print(f"{order}. roll = {random_num}")
+
+print(f"Total = {total}")
