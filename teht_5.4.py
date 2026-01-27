234315773fc86a78032fb7d74a8ad36495a82ef2
saif <saif@gmail.com>
2026-01-27 14:01:34 +0200

    city list

---
+++ teht_5.4.py
@@ -0,0 +1,12 @@
+
+city_list = []
+loop = 0
+while loop <= 4:
+    city = input("Give city name: ")
+    city_list.append (city)
+    loop = loop + 1
+for city in (city_list):
+    print(city)
+
+
+
